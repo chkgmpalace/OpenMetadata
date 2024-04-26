642d1345d8bd8292b9058d461cdc356bfb71820a
Sergey <86800204+chkgmpalace@users.noreply.github.com>
2024-04-26 13:16:32 +0300

    Update App.tsx

--- openmetadata-ui/src/main/resources/ui/src/App.tsx
+++ openmetadata-ui/src/main/resources/ui/src/App.tsx
@@ -15,7 +15,7 @@ import { isEmpty } from 'lodash';
 import React, { FC, useEffect } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { I18nextProvider } from 'react-i18next';
-import { HashRouter, Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import AppRouter from './components/AppRouter/AppRouter';
@@ -84,9 +84,7 @@ const App: FC = () => {
                               <ApplicationsProvider>
                                 <DomainProvider>
                                   <EntityExportModalProvider>
-                                    <HashRouter basename='/omd' >
-                                      <AppRouter />
-                                    </HashRouter>
+                                    <AppRouter />
                                   </EntityExportModalProvider>
                                 </DomainProvider>
                               </ApplicationsProvider>
